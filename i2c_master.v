012adcd2ed8472010242c3907eddab3f051c1093
Karthik B S <129792064+karthikkbs05@users.noreply.github.com>
2023-05-16 23:22:34 +0530

    Update i2c_master.v

--- i2c_master.v
+++ i2c_master.v
@@ -7,7 +7,7 @@
 
 module i2c_master(
     inout sda,
-    output scl,clk2mhz_dummy,
+    output scl,clk2mhz_dummy,rw,
     input clk100mhz,res,
     input [7:0]data_to_send, addr_to_send  // contains 5 bit slave address and 2 bit dummy and last bit says the read/write
 );
@@ -16,7 +16,7 @@ module i2c_master(
                 address_send = 4'b0011, slave_ack_init = 4'b0100, slave_ack = 4'b0101,
                 data_send_init_wait = 4'b0110, data_send_init = 4'b0111, data_send = 4'b1000,
                 data_ack_init = 4'b1001, data_ack = 4'b1010, stop_init = 4'b1011,
-                stop = 4'b1100 ;
+                stop = 4'b1100, data_read_init = 4'b1101, data_read = 4'b1110 ;
       
       reg [3:0]state = idle;
       
@@ -24,7 +24,7 @@ module i2c_master(
           sda_h=0,scl_h=0,
           sda_mode=1,scl_mode = 1,scl_toggle = 0;
       reg [7:0]addr_to_send_store;   
-      reg [7:0]data_to_send_store;
+      reg [7:0]data_to_send_store,data_read_store;
       
       always@(data_to_send or addr_to_send)
       begin
@@ -32,12 +32,13 @@ module i2c_master(
           data_to_send_store = data_to_send;
       end
       
+      assign rw = addr_to_send_store[0];
       
       integer count = 0 , bit_count = 8 , count_ack_wait = 4 , count_sda_wait = 2; 
       
       assign sda = sda_mode ? 1'bZ : sda_h;
       assign scl = scl_mode ? 1'bZ : scl_toggle ? clk2mhz : scl_h;
-   
+     
      always @(posedge clk100mhz)
      begin
      count <= count + 1;
@@ -122,7 +123,10 @@ module i2c_master(
                   begin
                    
                    if(sda == 0) // if(sda == 0 && addr_to_send_store[0] == 1)
-                    state <= data_send_init_wait;
+                     if(rw == 0)
+                        state <= data_send_init_wait;
+                     else 
+                        state <= data_read_init;
                    else
                     state <= idle;
                    
@@ -184,6 +188,26 @@ module i2c_master(
                   state <= data_send_init;
                  end
                 end
+      
+      data_read_init : begin
+                       sda_mode <= 1;
+                       sda_h <= 1;
+                       state <= data_read;
+                       end
+             
+      data_read : begin
+                      data_read_store[bit_count -1] <= sda;
+                      bit_count <= bit_count - 1;
+                      if(bit_count < 0)
+                       begin
+ //                    scl_toggle <= 0;
+                        bit_count <= 8;
+                        state <= stop_init;
+                       end
+                      else 
+                       state <= data_read;
+                  end
+                
                 
      stop_init : begin
                  count_sda_wait <= count_sda_wait - 1;          //to make sda high
